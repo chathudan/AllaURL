2ba7de536662ee8f735cae2e0a11e56d81b87106
Chatura Wijesinghe <chathura@typsy.com>
2025-01-20 11:21:06 +1100

    Added Identity server

---
+++ AllaURL.Identity/Config.cs
@@ -0,0 +1,38 @@
+﻿using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Test;
+using System.Security.Claims;
+
+namespace AllaURL.Identity;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+               new IdentityResources.OpenId(),
+               new IdentityResources.Profile()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+            {
+                  new ApiScope(name: "api", displayName: "AllaURL API")
+            };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+            new Client
+            {
+                ClientId = "allaurl-api",
+                ClientSecrets = { new Secret("your-secret".Sha256()) },
+                AllowedGrantTypes = GrantTypes.ClientCredentials, // Use the Authorization Code Flow
+                RedirectUris = { "http://localhost:5000/signin-oidc" }, // Redirect URL for your app
+                PostLogoutRedirectUris = { "http://localhost:5000/signout-callback-oidc" },
+                AllowedScopes = { "openid", "profile", "api" }, // Scopes the client can access
+                RequirePkce = true, // Use PKCE for better security
+                AllowAccessTokensViaBrowser = true
+            }
+        };
+
+}
---
+++ AllaURL.Identity/Program.cs
@@ -0,0 +1,23 @@
+﻿using AllaURL.Identity;
+
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+   
+    var app = builder
+        .ConfigureServices()
+        .ConfigurePipeline();
+    
+    app.Run();
+}
+catch (Exception ex)
+{
+    //Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    //Log.Information("Shut down complete");
+    //Log.CloseAndFlush();
+}
